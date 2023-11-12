29423430de86d65cbf58f931bcabe58d10f36af9
Mihaelaplam <mihaelaplam@gmail.com>
2023-11-12 10:35:01 +0200

    added comments, finished all functions, added instructions for each functions for ease of reading

--- index.js
+++ index.js
@@ -1,28 +1,61 @@
-/*The net total amount of Profit/Losses over the entire period.
 
-The average of the changes in Profit/Losses over the entire period.
+// Function to calculate the total change in profits
+function calculateTotalChange(finances) {
+  var totalChange = 0;
+  for (var i = 1; i < finances.length; i++) {
+    totalChange += finances[i][1] - finances[i - 1][1];
+  }
+  return totalChange;
+}
 
-You will need to track what the total change in profits is from month to month and then find the average.
+// Function to find the greatest increase in profits
+function findGreatestIncrease(finances) {
+  var greatestIncrease = 0;
+  var greatestIncreaseDate = "";
 
-(Total/(Number of months - 1))
+  for (var i = 1; i < finances.length; i++) {
+    var increase = finances[i][1] - finances[i - 1][1];
 
-The greatest increase in profits (date and amount) over the entire period.
+    if (increase > greatestIncrease) {
+      greatestIncrease = increase;
+      greatestIncreaseDate = finances[i][0];
+    }
+  }
 
-The greatest decrease in losses (date and amount) over the entire period.
-*/
+  return { date: greatestIncreaseDate, amount: greatestIncrease };
+}
 
-//The total number of months included in the dataset.
+// Function to find the greatest decrease in losses
+function findGreatestDecrease(finances) {
+  var greatestDecrease = 0;
+  var greatestDecreaseDate = "";
 
-function calculateUniqueMonths(finances) {
-  var uniqueMonths = new Set();
+  for (var i = 1; i < finances.length; i++) {
+    var decrease = finances[i][1] - finances[i - 1][1];
 
-  finances.forEach((entry) => {
-    var [month, year] = entry[0].split("-");
-    var monthYear = `${month}-${year}`;
-    uniqueMonths.add(monthYear);
-  });
+    if (decrease < greatestDecrease) {
+      greatestDecrease = decrease;
+      greatestDecreaseDate = finances[i][0];
+    }
+  }
 
-  return uniqueMonths.size;
+  return { date: greatestDecreaseDate, amount: greatestDecrease };
+}
+
+// Function to calculate the average change in profits
+function calculateAverageChange(finances) {
+  var totalChange = 0;
+  for (var i = 1; i < finances.length; i++) {
+    totalChange += finances[i][1] - finances[i - 1][1];
+  }
+  var numChanges = finances.length - 1;
+  var averageChange = totalChange / numChanges;
+  return averageChange;
+}
+
+// Function to get the total number of months
+function getTotalMonths(finances) {
+  return finances.length;
 }
 
 var finances = [
@@ -112,113 +145,31 @@ var finances = [
   ["Dec-2016", 60988],
   ["Jan-2017", 138230],
   ["Feb-2017", 671099],
-];
+]; 
 
-var totalMonths = calculateUniqueMonths(finances);
-console.log("Total number of months in the dataset:", totalMonths);
+var totalChange = calculateTotalChange(finances);
+var greatestIncrease = findGreatestIncrease(finances);
+var greatestDecrease = findGreatestDecrease(finances);
+var totalMonths = getTotalMonths(finances);
+var averageChange = calculateAverageChange(finances);
 
-
-// The net total amount of Profit/Losses over the entire period.
-function totalProfitLoss() {
-  let netTotal = 0;
-  var finances = [
-    ["Jan-2010", 867884],
-    ["Feb-2010", 984655],
-    ["Mar-2010", 322013],
-    ["Apr-2010", -69417],
-    ["May-2010", 310503],
-    ["Jun-2010", 522857],
-    ["Jul-2010", 1033096],
-    ["Aug-2010", 604885],
-    ["Sep-2010", -216386],
-    ["Oct-2010", 477532],
-    ["Nov-2010", 893810],
-    ["Dec-2010", -80353],
-    ["Jan-2011", 779806],
-    ["Feb-2011", -335203],
-    ["Mar-2011", 697845],
-    ["Apr-2011", 793163],
-    ["May-2011", 485070],
-    ["Jun-2011", 584122],
-    ["Jul-2011", 62729],
-    ["Aug-2011", 668179],
-    ["Sep-2011", 899906],
-    ["Oct-2011", 834719],
-    ["Nov-2011", 132003],
-    ["Dec-2011", 309978],
-    ["Jan-2012", -755566],
-    ["Feb-2012", 1170593],
-    ["Mar-2012", 252788],
-    ["Apr-2012", 1151518],
-    ["May-2012", 817256],
-    ["Jun-2012", 570757],
-    ["Jul-2012", 506702],
-    ["Aug-2012", -1022534],
-    ["Sep-2012", 475062],
-    ["Oct-2012", 779976],
-    ["Nov-2012", 144175],
-    ["Dec-2012", 542494],
-    ["Jan-2013", 359333],
-    ["Feb-2013", 321469],
-    ["Mar-2013", 67780],
-    ["Apr-2013", 471435],
-    ["May-2013", 565603],
-    ["Jun-2013", 872480],
-    ["Jul-2013", 789480],
-    ["Aug-2013", 999942],
-    ["Sep-2013", -1196225],
-    ["Oct-2013", 268997],
-    ["Nov-2013", -687986],
-    ["Dec-2013", 1150461],
-    ["Jan-2014", 682458],
-    ["Feb-2014", 617856],
-    ["Mar-2014", 824098],
-    ["Apr-2014", 581943],
-    ["May-2014", 132864],
-    ["Jun-2014", 448062],
-    ["Jul-2014", 689161],
-    ["Aug-2014", 800701],
-    ["Sep-2014", 1166643],
-    ["Oct-2014", 947333],
-    ["Nov-2014", 578668],
-    ["Dec-2014", 988505],
-    ["Jan-2015", 1139715],
-    ["Feb-2015", 1029471],
-    ["Mar-2015", 687533],
-    ["Apr-2015", -524626],
-    ["May-2015", 158620],
-    ["Jun-2015", 87795],
-    ["Jul-2015", 423389],
-    ["Aug-2015", 840723],
-    ["Sep-2015", 568529],
-    ["Oct-2015", 332067],
-    ["Nov-2015", 989499],
-    ["Dec-2015", 778237],
-    ["Jan-2016", 650000],
-    ["Feb-2016", -1100387],
-    ["Mar-2016", -174946],
-    ["Apr-2016", 757143],
-    ["May-2016", 445709],
-    ["Jun-2016", 712961],
-    ["Jul-2016", -1163797],
-    ["Aug-2016", 569899],
-    ["Sep-2016", 768450],
-    ["Oct-2016", 102685],
-    ["Nov-2016", 795914],
-    ["Dec-2016", 60988],
-    ["Jan-2017", 138230],
-    ["Feb-2017", 671099],
-  ];
-
-  for (let i = 0; i < finances.length; i++) {
-    netTotal += finances[i][1];
-  }
-
-  finances.forEach(entry => {
-    netTotal += entry[1];
-  });
-
-  console.log("Total Profit/Loss:", netTotal);
-}
-
-totalProfitLoss();
+// Console log display
+console.log("Financial Analysis");
+console.log("----------------------------");
+console.log("Total Months:", totalMonths);
+console.log("Total: $" + totalChange);
+console.log("Average Change: " + averageChange.toFixed(2));
+console.log(
+  "Greatest Increase in Profits/Losses: " +
+    greatestIncrease.date +
+    " ($" +
+    greatestIncrease.amount +
+    ")"
+);
+console.log(
+  "Greatest Decrease in Profits/Losses: " +
+    greatestDecrease.date +
+    " ($" +
+    greatestDecrease.amount +
+    ")"
+);
